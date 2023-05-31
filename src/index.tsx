3a1f6c4321b11ee14007fbd9b243d85540c7dc09
Narendra <personal.rendra@gmail.com>
2023-05-31 23:39:15 +0700

    [ADD] Cakra UI

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    primary: {
+      main: '#ED7801',
+      100: '#FAAF18',
+      200: '#F1BC87',
+    },
+    background: {
+      paper: '#FFF',
+      secondary: '#404040'
+    },
+    text: {
+      main: '#8B542F',
+      secondary: '#919191'
+    }
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
